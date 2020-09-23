d77b90eaf1fc4019f8cc575b18cd337b370c26b4
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2020-09-23 19:36:23 -0400

    main

---
+++ ardenware-portible.sh
@@ -0,0 +1,3 @@
+# This ia a very high portible os, to open, run with terminal!
+wget https://github.com/ArdenyUser/ware2.2/archive/master.zip
+wget https://github.com/ArdenyUser/ArdenWARE-git-2.2/archive/master.zip
---
+++ master-install.sh
@@ -0,0 +1,15 @@
+# Below is the unzip installer for the system.
+# Below is very well needed.
+sudo apt-get install unzip
+# This will install main system features.
+# This installs some stuff from the web.
+# Please run this while os is running.
+echo Broading master installion...
+echo Echoing to web server...
+# Above is the main informer, it is not needed.
+# Below is the item installer, if you want, you can toy with it.
+# Note: This installs items for ArdenWARE 2.2 not this os, which may cause issues.
+wget https://github.com/ArdenyUser/ware3.5/archive/master.zip
+unzip ware3.5-master.zip
+mkdir SetUp
+# This will also create a dir called SetUp.
